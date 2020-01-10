5c15998ca967b89f57578baba794d167928f6677
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-10 16:39:14 +0100

    Added Routes constants in the routes.js file

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,1 +1,8 @@
 // The routes for each page in the application lives here using React-Router
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
+export const PASSWORD_FORGET = '/pw-forget';
