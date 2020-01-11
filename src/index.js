451ebeb2c32a4f6f862bb63974fbe9805bbecf72
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-11 22:08:16 +0100

    Added firebase instance Provider in index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Firebase, { FirebaseContext } from './components/Firebase';
 
+const firebaseInstance = new Firebase();
+
 const rootApp = (
-  
+  <FirebaseContext.Provider value={firebaseInstance}>
+    <App />
+  </FirebaseContext.Provider>
 )
 
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(rootApp, document.querySelector('#root'));
