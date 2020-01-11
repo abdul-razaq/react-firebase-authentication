c48565caa4ba482e00c594d4336d4eccbf9b96bd
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-11 04:44:06 +0100

    Added firebase context

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+const rootApp = (
+  
+)
 
 
 ReactDOM.render(<App />, document.querySelector('#root'));
