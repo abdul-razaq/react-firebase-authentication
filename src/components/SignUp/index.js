df9f54630d15957bb71277a83b2f2de9b70cc1e2
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-11 22:37:48 +0100

    Worked with signUp component

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -27,10 +27,10 @@ class SignUpForm extends Component {
   }
 }
 
-const SignUpLink = () => {
+const SignUpLink = () => (
   <p>
     Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
   </p>
-}
+)
 
 export default SignUp;
