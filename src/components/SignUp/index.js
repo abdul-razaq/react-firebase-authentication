79b4ded9b3ce0b13bf431bf811673b59cc7845d0
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-11 22:32:54 +0100

    Added signUp logic

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+/* eslint-disable no-useless-constructor */
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
+import * as ROUTES from '../../constants/routes';
 
 const SignUp = () => (
   <div>
-    <h1>Sign Up</h1>
+    <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+onSubmit = event => {
+
+}
+
+onChange = event => {
+
+}
 
 export default SignUp;
