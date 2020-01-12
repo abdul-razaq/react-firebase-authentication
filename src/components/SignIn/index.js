807f55c0bdf8c1c03449ee7165c674ed644e9de2
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-12 21:25:54 +0100

    Completed SignIn business Logic

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -6,7 +6,6 @@ import { SignUpLink } from '../SignUp';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
-
 const SignInPage = () => (
   <div>
     <h1>Sign In</h1>
@@ -16,5 +15,61 @@ const SignInPage = () => (
 );
 
 const INITIAL_STATE = {
-  
+  email: '',
+  password: '',
+  error: null,
+};
+
+class SignInFormBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    const { email, password } = this.state;
+
+    this.props
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => this.setState({ error }));
+
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { email, password, error } = this.state;
+
+    const isInvalid = password === '' || email === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          placeholder="Email Address"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={this.onChange}
+          placeholder="Enter Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign In
+        </button>
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
 }
