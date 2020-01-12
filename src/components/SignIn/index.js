e233ba02ef6c9f2c5f915531708491bfb7ee313a
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-12 21:15:29 +0100

    added some SignIn logic

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import { SignUpLink } from '../SignUp';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
 
 
-const SignIn = () => (
+const SignInPage = () => (
   <div>
     <h1>Sign In</h1>
+    <SignInForm />
+    <SignUpLink />
   </div>
 );
 
-export default SignIn;
+const INITIAL_STATE = {
+  
+}
