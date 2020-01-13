f532e27a3aa62d086572ea76d8197f3ed604f705
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-13 21:48:08 +0100

    Passed currently authenticated user state to Navigation component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -18,7 +18,7 @@ const App = () => {
   return (
     <Router>
       <div>
-        <Navigation />
+        <Navigation authUser={authUser} />
         <hr />
 
         <Route exact path={ROUTES.LANDING} component={LandingPage} />
