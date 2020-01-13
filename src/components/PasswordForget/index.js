ad7c4cf01c596a35d0bd18ccdf35a40c61f31678
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-13 23:42:57 +0100

    Added PasswordForget functionality

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
 
-const PasswordForget = () => (
+const PasswordForgetPage = () => (
   <div>
     <h1>Password Forget</h1>
+    <PasswordForgetForm />
   </div>
 );
 
-export default PasswordForget;
+const INITIAL_STATE = {
+  email: '',
+  error: null,
+};
+
+class PasswordForgetFormBase extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    const { email } = this.state;
+
+    this.props.firebase
+      .doPasswordReset(email)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+      })
+      .catch(error => this.setState({ error }));
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { email, error } = this.state;
+
+    const isInvalid = error === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter email address"
+          onChange={this.onChange}
+          value={this.state.email}
+        />
+        <button type="submit" disabled={isInvalid}>
+          Reset My Password
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
