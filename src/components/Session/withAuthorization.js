1df018a8a4620feb200e15b494aaede9a523e399
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-25 12:31:36 +0100

    Added withAuthorization HOC logic

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
-const withAuthorization = () => Component => {
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+const withAuthorization = condition => Component => {
   class WithAuthorization extends React.Component {
+    componentDidMount() {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+        if (!condition(authUser)) {
+          this.props.history.push(ROUTES.SIGN_IN);
+        }
+      });
+    }
+
+    componentWillMount() {
+      this.listener();
+    }
+
     render() {
       return <Component {...this.props} />;
     }
   }
 
-  return WithAuthorization;
+  return compose(withRouter, withFirebase)(withAuthorization);
 };
 
 export default withAuthorization;
