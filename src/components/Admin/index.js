7fbe94e2d7beb32c850581a6d9fa4c5b116b376e
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-25 15:59:21 +0100

    used the Admin component to fetch all users from the realtime database

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 
+import { withFirebase } from '../Firebase';
 
-const Admin = () => (
-  <div>
-    <h1>Admin</h1>
-  </div>
-);
+const AdminPage = ({ firebase }) => {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState({});
 
-export default Admin;
+  useEffect(() => {
+    setLoading(true);
+
+    firebase.users().on('value', snapshot => {
+      setUsers(snapshot.val());
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Admin</h1>
+    </div>
+  );
+};
+
+export default withFirebase(AdminPage);
